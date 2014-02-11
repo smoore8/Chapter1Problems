21631a095adb91540807fced331e4e368eb7bfb7
smoore8 <savionmoore8@gmail.com>
2014-02-11 10:02:31 -0500

    Initial

--- Chapter1/Address/Program.cs
+++ Chapter1/Address/Program.cs
@@ -3,18 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace ConsoleApplication1
+namespace Address
 {
     class Program
     {
         static void Main(string[] args)
         {
-
-         
         }
     }
-
-   
 }
-
-    
