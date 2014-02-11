e8161473d4a96284357ef554d0f8e9c412caa0d4
smoore8 <savionmoore8@gmail.com>
2014-02-11 09:48:20 -0500

    Savion

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for(int i=1; i<=100; i++)
+            {
+                if( i % 10 == 0)
+                    Console.WriteLine(i);
+
+            }
+        }
+    }
+
+   
+}
+
+    
