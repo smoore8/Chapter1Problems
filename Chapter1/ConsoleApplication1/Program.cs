c88f703cf3499018341038a4eea6a2971c32c86a
smoore8 <savionmoore8@gmail.com>
2014-02-12 10:11:44 -0500

    Chapter 1

---
+++ Chapter1/ConsoleApplication1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("    X    ");
+            Console.WriteLine("   XXX   ");
+            Console.WriteLine("  XXXXX  ");
+            Console.WriteLine(" XXXXXXX ");
+            Console.WriteLine("    X    ");
+        }
+    }
+}
---
+++ Chapter1/ConsoleApplication2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("sssss m       m");
+            Console.WriteLine("s     m m   m m");
+            Console.WriteLine("sssss m   m   m");
+            Console.WriteLine("    s m       m");
+            Console.WriteLine("sssss m       m");
+        }
+    }
+}
